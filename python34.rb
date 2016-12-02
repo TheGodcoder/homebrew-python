544b9c8bbf7afa8c15653540cd56c0bb4b0cdc5e
TheGodcoder <mrmarksearle@gmail.com>
2016-12-02 17:27:29 +0000

    Update python34.rb
    Updating to install Python 3.4.1

--- python34.rb
+++ python34.rb
@@ -1,9 +1,9 @@
 class Python34 < Formula
   desc "Interpreted, interactive, object-oriented programming language"
   homepage "https://www.python.org/"
-  url "https://www.python.org/ftp/python/3.4.4/Python-3.4.4.tgz"
-  version "3.4.4"
-  sha256 "bc93e944025816ec360712b4c42d8d5f729eaed2b26585e9bc8844f93f0c382e"
+  url "https://www.python.org/ftp/python/3.4.1/Python-3.4.1.tgz"
+  version "3.4.1"
+  sha256 "8d007e3ef80b128a292be101201e75dec5480e5632e994771e7c231d17720b66"
 
   def install
     system "./configure", "--prefix=#{prefix}"
